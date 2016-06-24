85335e0a185b905216bd9aa576d39dc83a301a32
haena612 <haena612@gmail.com>
2016-06-25 03:27:16 +0900

    Update ViewController.swift

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -16,22 +16,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-    
-
+    //Set variables
+   
     @IBOutlet weak var TextField: UITextField!
+    // customers can fill out this text field about the restaurent's service
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
         let DestViewController:SettingViewController = segue.destinationViewController as! SettingViewController
+        // create a variable in SettingViewController class 
         
         DestViewController.LabelText = TextField.text!
+        // set DestViewController (SettingViewController class) to be the input value from the textfield
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
+        
+        //set initial values for tip and total as 0
         
     }
 
@@ -42,20 +49,24 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onEditingChange(sender: AnyObject) {
-
-        
-        
+    
         var tipPercentages = [0.18, 0.2, 0.25]
+        // Set an array with three tip percentage values
         let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        // Set a new variable for tipPercentage values depending on user's selection among three array values
         let billAmount = NSString(string: billField.text!).doubleValue
+        // Set a new variable to have the input value from the bill 
         let tip = billAmount * tipPercentage
         let total = billAmount + tip
+        // Calculations
         
         tipLabel.text = "$\(tip)"
         totalLabel.text = "$\(total)"
+        //Display the Calculated Values
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        // Display upto 2 decimal places
     }
     
     @IBAction func onTap(sender: AnyObject) {
